43ed8c96e43c78b48e40ad759c90b8590de4ce6a
Aaron France <aaron.l.france@gmail.com>
2020-02-18 02:16:01 +0100

    fix: faceroll

--- main.go
+++ main.go
@@ -12,10 +12,9 @@ import (
 )
 
 type IOData struct {
-	read                int
+	read                C.int
 	firstOffset, offset C.off_t
 	firstLen            C.size_t
-	iov                 C.struct_iovec
 }
 
 type Ring struct {
@@ -35,11 +34,30 @@ func (r *Ring) QueueRead(fd uintptr, size uint64, offset uint64) {
 	data.firstOffset = C.long(offset)
 	data.firstLen = C.ulong(size)
 
-	data.iov.iov_base = unsafe.Pointer(&data.read)
-	data.iov.iov_len = C.ulong(size)
+	iov := (*C.struct_iovec)(C.malloc(C.size_t(unsafe.Sizeof(C.struct_iovec{}))))
+	if iov == nil {
+		panic("LOL")
+	}
 
-	C.io_uring_prep_readv(sqe, C.int(fd), &data.iov, 1, C.long(offset))
-	C.io_uring_sqe_set_data(sqe, unsafe.Pointer(&data.iov))
+	iov.iov_base = unsafe.Pointer(&data.firstOffset)
+	iov.iov_len = C.ulong(size)
+
+	C.io_uring_prep_readv(sqe, C.int(fd), iov, 1, C.long(offset))
+	C.io_uring_sqe_set_data(sqe, unsafe.Pointer(iov))
+	C.io_uring_submit(&r.ring)
+
+	// Something like this should be used: figure out how to do it.
+	// var cqe []C.struct_io_uring_cqe
+	// if C.io_uring_wait_cqe(&r.ring, &cqe) < 0 {
+	// 	panic("lolno")
+	// }
+
+	// just print the iovec to stdout, works because the iovec is
+	// what's populated by the actual io_yourring shit.
+	//
+	// the iovec sometimes has no data, because async - run it a few
+	// times.
+	C.writev(1, iov, 3)
 }
 
 func (r *Ring) Init() error {
@@ -52,7 +70,7 @@ func (r *Ring) Init() error {
 func main() {
 	r := &Ring{}
 	fmt.Println(r.Init())
-	f, err := os.Open("/dev/null")
+	f, err := os.Open("/dev/urandom")
 	if err != nil {
 		fmt.Println(err)
 	}
